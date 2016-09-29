dd7ba528bbf967de6b92d1316a4f11080edb5d85
Teerachai <teerachai@perfenterprise.com>
2016-09-29 12:44:55 +0700

    Pair with Application Insights.
    Signed-off-by: Teerachai <teerachai@perfenterprise.com>

--- Workshop1/WebSite2Test/App_Start/FilterConfig.cs
+++ Workshop1/WebSite2Test/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace WebSite2Test
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
